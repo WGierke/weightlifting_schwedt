0b721b26157b4d89d31bf68ffac1d4cbbdcf6ac9
willi.gierke <willi.gierke@student.hpi.uni-potsdam.de>
2016-02-10 22:44:09 +0100

    create tokens at app engine via POST

--- server/schwedt_app_engine/schwedt_app_engine.py
+++ server/schwedt_app_engine/schwedt_app_engine.py
@@ -1,10 +1,63 @@
+import cgi
+import urllib
+
+from google.appengine.api import users
+from google.appengine.ext import ndb
+from google.appengine.ext import db
+
 import webapp2
+
+DEFAULT_TOKEN_VALUE = 'default_token'
+
+
+def token_key(token_value=DEFAULT_TOKEN_VALUE):
+    return ndb.Key('Token', token_value)
+
+
+class Token(ndb.Model):
+    """A GCM token to send the app users push notifications"""
+    value=ndb.StringProperty(indexed=False)
+
+
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Sup')
+        token_query = Token.query(ancestor=token_key(DEFAULT_TOKEN_VALUE))
+        tokens = token_query.fetch(100)
+        self.response.write(str(len(tokens)))
+        self.response.out.write('''
+        <html>
+          <body>
+            <form method="post" action="/add_token">
+              <p>Name: <input type="text" name="token" /></p>
+              <p><input type="submit" /></p>
+            </form>
+          </body>
+        </html>
+        ''')
+
+        for token in tokens:
+            self.response.write('<b>%s</b> wrote:' % token)
+
+
+class AddToken(webapp2.RequestHandler):
+    def post(self):
+        self.response.write('end')
+        value = self.request.get('token')
+        self.response.write(value)
+        token = Token(parent=token_key(DEFAULT_TOKEN_VALUE))
+        token.value = value
+        token.put()
+
+
+class DeleteToken(webapp2.RequestHandler):
+    def delete(self):
+        value = self.request.get('token')
+        self.response.write(value)
+        db.delete(db.Key.from_path('Token', value))
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/add_token', AddToken),
+    ('/delete_token', DeleteToken),
 ], debug=True)
