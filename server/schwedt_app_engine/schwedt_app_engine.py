8b2e28caf034bca8cc61fbc4daa5a14bda8d3a6c
willi.gierke <willi.gierke@student.hpi.uni-potsdam.de>
2016-02-18 16:02:22 +0100

    manage tokens at app engine with SECRET KEY

--- server/schwedt_app_engine/schwedt_app_engine.py
+++ server/schwedt_app_engine/schwedt_app_engine.py
@@ -1,11 +1,8 @@
-import cgi
-import urllib
-
-from google.appengine.api import users
 from google.appengine.ext import ndb
-from google.appengine.ext import db
 
 import webapp2
+import json
+import os
 
 DEFAULT_TOKEN_VALUE = 'default_token'
 
@@ -14,50 +11,56 @@ def token_key(token_value=DEFAULT_TOKEN_VALUE):
     return ndb.Key('Token', token_value)
 
 
+def valid_secret_key(request):
+    return 'X-Secret-Key' in request.headers and request.headers["X-Secret-Key"] == os.environ.get("SECRET_KEY")
+
+
 class Token(ndb.Model):
     """A GCM token to send the app users push notifications"""
-    value=ndb.StringProperty(indexed=False)
-
+    value = ndb.StringProperty(indexed=False)
 
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        token_query = Token.query(ancestor=token_key(DEFAULT_TOKEN_VALUE))
-        tokens = token_query.fetch(100)
-        self.response.write(str(len(tokens)))
-        self.response.out.write('''
-        <html>
-          <body>
-            <form method="post" action="/add_token">
-              <p>Name: <input type="text" name="token" /></p>
-              <p><input type="submit" /></p>
-            </form>
-          </body>
-        </html>
-        ''')
+        if valid_secret_key(self.request):
+            self.response.out.write('Valid Secret Key - nice!')
+        else:
+            self.response.out.write('Secret Key is not valid')
 
-        for token in tokens:
-            self.response.write('<b>%s</b> wrote:' % token)
+
+class GetTokens(webapp2.RequestHandler):
+    def get(self):
+        if valid_secret_key(self.request):
+            token_query = Token.query()
+            tokens = token_query.fetch(1000)
+            response_dict = {}
+            response_dict["result"] = map(lambda (x): x.value, tokens)
+            self.response.write(json.dumps(response_dict, encoding='latin1'))
 
 
 class AddToken(webapp2.RequestHandler):
     def post(self):
-        self.response.write('end')
-        value = self.request.get('token')
-        self.response.write(value)
-        token = Token(parent=token_key(DEFAULT_TOKEN_VALUE))
-        token.value = value
-        token.put()
+        if valid_secret_key(self.request):
+            value = self.request.get('token')
+            token = Token(parent=token_key(value))
+            token.value = value
+            token.put()
+            self.response.write('Success')
 
 
 class DeleteToken(webapp2.RequestHandler):
-    def delete(self):
-        value = self.request.get('token')
-        self.response.write(value)
-        db.delete(db.Key.from_path('Token', value))
+    def post(self):
+        if valid_secret_key(self.request):
+            value = self.request.get('token')
+            token_query = Token.query(ancestor=token_key(value))
+            tokens = token_query.fetch(100)
+            for token in tokens:
+                token.key.delete()
+            self.response.write('Success')
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/add_token', AddToken),
     ('/delete_token', DeleteToken),
+    ('/get_tokens', GetTokens),
 ], debug=True)
